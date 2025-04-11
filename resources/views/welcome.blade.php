6a70a7896bc9ed095d7a025c4b2b87108ce03473
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-04-11 18:21:57 +0500

    add tags under Tag section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,18 @@
 
         <section>
             <x-section-heading>Tags</x-section-heading>
+
+            <div class="mt-6 space-x-1">
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+            </div>
         </section>
 
         <section>
