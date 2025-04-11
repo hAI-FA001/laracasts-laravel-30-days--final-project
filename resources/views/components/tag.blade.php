b671b73c35e81d24d6e0169df567ee3b36dc3610
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-04-11 18:19:29 +0500

    add tag component

---
+++ resources/views/components/tag.blade.php
@@ -0,0 +1,4 @@
+{{-- Tip: transition-colors and duration-300 for a smoother transition --}}
+<a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">
+    {{ $slot }}
+</a>
