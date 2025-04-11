9e24f1555d2501a7cf2569271b272786443c2918
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-04-11 17:45:58 +0500

    reference logo with Vite::asset()

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,12 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
+    @vite(['resources/js/app.js'])
 </head>
 
 <body>
     <div>
         <nav>
-            <div>logo</div>
+            <div>
+                <a href="">
+                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
+                </a>
+            </div>
             <div>links</div>
             <div>post a job</div>
         </nav>
