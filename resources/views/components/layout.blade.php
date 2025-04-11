af74a4da5b40adc30cb5c3368b1425cefbc45245
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-04-11 17:42:30 +0500

    add logo and initial layout

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pixel Positions</title>
+</head>
+
+<body>
+    <div>
+        <nav>
+            <div>logo</div>
+            <div>links</div>
+            <div>post a job</div>
+        </nav>
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+</body>
+
+</html>
