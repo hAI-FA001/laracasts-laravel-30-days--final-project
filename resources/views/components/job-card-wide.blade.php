e2dd431133ec301a8b40d250b30ebf184a8c018a
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-05-19 16:33:08 +0500

    add wide job card

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,20 @@
+<div class="p-4 bg-white/5 rounded-xl flex">
+
+    {{-- wrap img in a div, direct children of flex gave gigantic images --}}
+    <div>
+        <img src="http://place-hold.it/42/42" alt="" class="rounded-xl">
+    </div>
+
+    <div>
+        <a href='#' class="self-start text-sm">Laracasts</a>
+
+        <h3>Video Producer</h3>
+        <p>Full Time - From $60,000</p>
+    </div>
+
+    <div>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+    </div>
+</div>
