c7cf9da51e79e98817a350cba3519c2ccd2437fc
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-05-19 18:25:19 +0500

    add hover effects, use group and group-hover for linking child with ancestor's hover

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,9 +1,10 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+<div
+    class="p-4 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-800 group transition-colors duration-300">
     <div class="self-start text-sm">Laracasts</div>
 
-    <div class="py-8 font-bold">
-        <h3>Video Producer</h3>
-        <p>Full Time - From $60,000</p>
+    <div class="py-8">
+        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">Video Producer</h3>
+        <p class="text-sm mt-4">Full Time - From $60,000</p>
     </div>
 
     {{-- mt-auto will force this to be at the bottom in cases where there's some more margin to be had --}}
