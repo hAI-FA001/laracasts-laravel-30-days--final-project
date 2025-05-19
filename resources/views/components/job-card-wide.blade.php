7c1557305e6d033eb5d58aa688f1a1f80eee5696
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-05-19 17:12:57 +0500

    adjust texts

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -5,11 +5,11 @@
         <img src="http://picsum.photos/seed/{{ rand(0, 100000) }}/100/100" alt="" class="rounded-xl">
     </div>
 
-    <div class='flex-1'>
-        <a href='#' class="self-start text-sm">Laracasts</a>
+    <div class='flex-1 flex flex-col'>
+        <a href='#' class="self-start text-sm text-gray-400">Laracasts</a>
 
-        <h3>Video Producer</h3>
-        <p>Full Time - From $60,000</p>
+        <h3 class="font-bold text-xl mt-3">Video Producer</h3>
+        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
     </div>
 
     <div>
