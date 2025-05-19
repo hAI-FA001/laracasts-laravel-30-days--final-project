9fb76c3975d950ec43c7aa16bc38fd153fb582e9
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-05-19 16:36:42 +0500

    adjust spacing and flex, use unique images from picsum instead of placeholder

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,11 +1,11 @@
-<div class="p-4 bg-white/5 rounded-xl flex">
+<div class="p-4 bg-white/5 rounded-xl flex gap-x-6">
 
     {{-- wrap img in a div, direct children of flex gave gigantic images --}}
     <div>
-        <img src="http://place-hold.it/42/42" alt="" class="rounded-xl">
+        <img src="http://picsum.photos/seed/{{ rand(0, 100000) }}/100/100" alt="" class="rounded-xl">
     </div>
 
-    <div>
+    <div class='flex-1'>
         <a href='#' class="self-start text-sm">Laracasts</a>
 
         <h3>Video Producer</h3>
