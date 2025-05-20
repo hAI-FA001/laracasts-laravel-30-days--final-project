e5a23ebf0f794d769f3b6ebb9d7ea53eb728c6a8
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-05-20 15:28:19 +0500

    add JobTest, setup Pest to refresh database cuz we want to interact with models, Test-Driven Development (TDD)

--- tests/Unit/JobTest.php
+++ tests/Unit/JobTest.php
@@ -1,5 +1,18 @@
 <?php
 
-test('example', function () {
-    expect(true)->toBeTrue();
+use App\Models\Employer;
+use App\Models\Job;
+
+test('it belongs to employer', function () {
+    // Arrange, Act, Assert - AAA
+
+    // 
+    $employer = Employer::factory()->create();
+    $job = Job::factory()->create([
+        'employer_id' => $employer->id,
+    ]);
+
+    // 
+    // TDD: we would need to add the relation/function in models if we didn't cuz the test would fail then
+    expect($job->employer->is($employer))->toBeTrue();
 });
