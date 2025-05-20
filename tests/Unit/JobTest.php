70ff6b5f76f0b7910d8e01c19b0134b9a9eb5883
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-05-20 15:17:32 +0500

    add unit test - check job belongs to employer, gives facade root error

--- tests/Unit/JobTest.php
+++ tests/Unit/JobTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit;
 
+use App\Models\Employer;
+use App\Models\Job;
 use PHPUnit\Framework\TestCase;
 
 class JobTest extends TestCase
@@ -9,8 +11,17 @@ class JobTest extends TestCase
     /**
      * A basic unit test example.
      */
-    public function test_example(): void
+    public function test_it_belongs_to_an_employer(): void
     {
-        $this->assertFalse(true);
+        // Arrange, Act, Assert -> AAA
+
+        // 
+        $employer = Employer::factory()->create();
+        $job = Job::factory()->create([
+            'employer_id' => $employer->id,
+        ]);
+
+        // 
+        $this->assertTrue($job->employer->is($employer));
     }
 }
