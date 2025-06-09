6f4a835bc82d03a1361eafcb21dfa4ef7c23de08
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-06-10 00:49:02 +0500

    finish up store(), cleanup unneeded routes - uploadedFile in laravel, storage system and filesystems config file

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,18 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\File;
+use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
     /**
      * Show the form for creating a new resource.
      */
@@ -27,38 +23,31 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        $userAttrs = $request->validate([
+            'name' => ['required'],
+            'email'=>['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'confirmed', Password::min(6)],
+        ]);
+        
+        $employerAttrs = $request->validate([
+            'employer' => ['required'],
+            'logo' => ['required', File::types(['png', 'jpg', 'webp'])],
+            
+        ]);
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
+        // benefit of splitting user and employer validation
+        $user = User::create($userAttrs);
+        
+        // configure in config/filesystems.php, includes options like local storage, S3, FTP etc
+        // change FILESYSTEM_DISK env var to public, which will store it under storage/app/public and make a symbolic link in public/
+        $logoPath = $request->logo->store('logos');
+        $user->employer->create([
+            'name' => $employerAttrs['employer'],
+            'logo' => $logoPath,
+        ]);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
+        Auth::login($user);
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return redirect('/');
     }
 }
